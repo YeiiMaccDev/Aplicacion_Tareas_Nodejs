088d1e57d638c47a37508e4e64b2b9ef84e902ba
YeiiMacc <yeiimaccdev@gmail.com>
2022-04-18 00:59:32 -0500

    Menu structure

--- app.js
+++ app.js
@@ -1,9 +1,18 @@
 require('colors');
 
+const { showMenu, pause } = require('./helpers/messages');
+
 console.clear();
 
 const main = async() => {
-    console.log(`Hello world`);
+
+    let opt = ``;
+
+    do {
+        opt = await showMenu();
+        console.log({ opt });
+        await pause();
+    } while (opt !== '0');
 }
 
 main();
